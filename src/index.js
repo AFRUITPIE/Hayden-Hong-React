85c79cd3278c9c946b3a2839fb312f1d21ebce3d
Hayden Hong <afruitpie@gmail.com>
2017-10-16 22:26:54 -0700

    Figured out typing animation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let bodyText = "Hello, my name is Hayden Hong. I'm a student at the University of Washington and I'm a piece of shit. AMA. ";
+
+renderText(0);
+
+function renderText(i) {
+
+
+    let toPrint = bodyText.substring(0, i);
+
+    for (let j = 0; j < bodyText.length - i; j++) {
+        toPrint = toPrint + "-";
+    }
+
+    ReactDOM.render(<p className="body">{toPrint}</p>, document.getElementById('root'));
+
+    if (i < bodyText.length) {
+        setTimeout(function() {renderText(i + 1)}, 50);
+    }
+}
+
 registerServiceWorker();
