151eeab64a6264b4bee2d806a250aeff4fc98d60
Hayden Hong <hello@haydenhong.com>
2018-09-15 12:28:45 -0700

    Created site for Convertify

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import LineFlowAnimation from "./lineFlowAnimation";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <div>
+  <div className="body-text">
+    <img src="Icon.png" width="50" alt="app icon" />
     <LineFlowAnimation
       lines={[
-        "Hello.",
-        "My name is Hayden Hong.",
-        "I am a student at the University of Washington.",
-        "I love to code and I am passionate about software design."
+        "Convertify - Share Music",
+        "Open music in the correct app.",
+        "Just copy a link to Apple Music to open in Spotify,",
+        "or copy a link from Spotify to open in Apple Music."
       ]}
     />
-    <a href="https://github.com/AFRUITPIE">GitHub</a>
-    <a href="https://www.linkedin.com/in/hayden-hong-software/">LinkedIn</a>
-    <a href="https://itunes.apple.com/us/app/convertify-share-music/id1424728187">Convertify iOS</a>
+    <a href="https://itunes.apple.com/us/app/convertify-share-music/id1424728187">App Store</a>
+    <a href="http://haydenhong.com/convertify/privacy-policy/">Privacy Policy</a>
   </div>,
   document.getElementById("root")
 );
